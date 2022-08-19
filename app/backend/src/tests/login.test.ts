4b0862d8048e3d84725259218630531cbf2b44ed
Gabriel <gabrielferrari601@gmail.com>
2022-08-19 12:14:54 -0300

    Test(POST /login): Testa sucesso e campos vazios

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,5 +1,6 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import * as bcrypt from 'bcryptjs';
 
 // @ts-ignore
 import chaiHttp = require('chai-http');
@@ -15,40 +16,61 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('/login', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+const validAdmin = {id: 1, username:' Admin', role: 'admin', email:'admin@admin.com', password:'$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW' };
+const validUser =  {id:2, username: 'User', role:'user', email: 'user@user.com', password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'};
 
-  let chaiHttpResponse: Response;
+let chaiHttpResponse: Response;
 
-  before(async () => {
-  //   sinon
-  //     .stub(Users as unknown as Model, "set")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Users);
-  // });
-  // sinon
-  //     .stub(Users as unknown as Model, "set")
-  //     .resolves();
+describe('POST /login on success', () => {
+
+  beforeEach(async () => {
+    sinon
+      .stub(Users, "set")
+      .resolves(null as typeof Users | null);
+    sinon
+    .stub(bcrypt, 'compare')
+    .resolves(true);
   });
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+  afterEach(()=>{
+    sinon.restore();
+  })
 
   it('should return a status 200', async () => {
     chaiHttpResponse = await chai
       .request(app)
-      .post('/login').send();
+      .post('/login').send({
+        email: validAdmin.email,
+        password: 'secret_admin'
+      });
     expect(chaiHttpResponse.status.valueOf()).to.be.equal(200)
   });
 
   it('should return a token', async () => {
     chaiHttpResponse = await chai
       .request(app)
-      .post('/login');
-    expect(chaiHttpResponse.body).to.haveOwnProperty('token')
+      .post('/login').send({
+        email: validAdmin.email,
+        password: 'secret_admin'
+      });;
+    expect(chaiHttpResponse.body).to.haveOwnProperty('token');
   });
 });
+
+describe('POST /login on fail', () => {
+  describe('when the fields are empty', () => {
+    beforeEach( async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login').send();
+    })
+    afterEach(sinon.restore)
+    it('should return status 400 on empty fields', () => {
+      expect(chaiHttpResponse.status.valueOf()).to.be.equal(400);
+    })
+    it('should return an object with the message "All fields must be filled"', () => {
+      expect(chaiHttpResponse.body).to.haveOwnProperty("message");
+      expect(chaiHttpResponse.body.message).to.be.equal("All fields must be filled");
+    })
+  })
+  })
